6a1d79c41f9c598941d7b8f00cb64f9a2217e5ef
Louis Xu <louisxu2003@gmail.com>
2023-11-24 03:14:41 -0500

    removed header

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import VisualMemoryPage from "./assets/pages/VisualMemoryPage";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Header from "./assets/components/header";
 import Home from "./assets/pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header></Header>
         <Routes>
           <Route path="/" element={<Home></Home>} />
           <Route path="/VisualMemory" element={<VisualMemoryPage />} />
